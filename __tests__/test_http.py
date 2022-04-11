60febfeaea82e4494f3ee02e741b8e34f3ef25ea
Mao De Matos <mao.dematos@gmail.com>
2022-04-11 17:21:33 +0200

    🐛 Fix `app` broken import

--- __tests__/test_http.py
+++ __tests__/test_http.py
@@ -1,9 +1,14 @@
-import requests
+import pytest
 import os
 
-app_url = os.environ["APP_URL"] + ":" + os.environ["APP_PORT"]
+from app import app
+
+# app_url = os.environ["APP_URL"] + ":" + os.environ["APP_PORT"]
+
+# test_app = Flask(__name__)
 
 
+@pytest.mark.get_request
 def test_flask_is_started():
-    response = requests.get(app_url)
+    response = app.test_client().get("/")
     assert response.status_code == 200
