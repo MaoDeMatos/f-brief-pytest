51f472a9fd276e605804ea129249281698adfb6d
Mao De Matos <mao.dematos@gmail.com>
2022-04-14 11:36:04 +0200

    ✨ Add `locust` load test

---
+++ locustfile.py
@@ -0,0 +1,27 @@
+from random import random
+from locust import HttpUser, between, task
+
+
+class User(HttpUser):
+    wait_time = between(0.1, 0.5)
+
+    @task(4)
+    def heavy_load(self):
+        self.client.get("")
+        self.client.get("cats")
+
+    # /other
+
+    @task
+    def page_other(self):
+        self.client.get("other")
+        number = random()
+        self.client.get(f"/other?page={number}", name="/other")
+
+    # /exp
+
+    @task
+    def page_exp(self):
+        self.client.get("exp")
+        number = random()
+        self.client.get(f"/exp?value={number}", name="/exp")
